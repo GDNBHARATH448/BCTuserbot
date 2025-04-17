cebd35a26f5fc90b3860c4eba9c0ad08b6b12f6a
Chinna op <opchinna742@gmail.com>
2025-04-17 11:42:33 +0530

    Update adminHelpers.py

--- Chinnaop/helper/adminHelpers.py
+++ Chinnaop/helper/adminHelpers.py
@@ -4,7 +4,7 @@ from time import time
 from pyrogram.types import Message
 
 from pyrogram import Client 
-from UTTAM.helper.interval import IntervalHelper
+from Chinnaop.helper.interval import IntervalHelper
 
 
 async def CheckAdmin(message: Message):
