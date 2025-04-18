b6c30325e2428100542ea4b3d17bcc942a8ab7cc
Chinna op <opchinna742@gmail.com>
2025-04-18 14:28:56 +0530

    Update config (6).py

--- config (6).py
+++ config (6).py
@@ -16,12 +16,12 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7500951257:AAFwz8XYQ_bFBjiqcFeE9yN0QfU3-Kl6gik"
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/w04ren.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP", "1002141779241")
+LOG_GROUP = getenv("LOG_GROUP", "-1002532559194")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/GDNBharat/Toxic-userbot")
 BRANCH = getenv("BRANCH", "main") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQGwyGIANybBz7aR5hdkPvgawzfLwHU5lCH3VqEYAZNU1RiMNUGOA8Xx5ScMK5Yk8MDuYAGyaroa2AvWIy2AzQkEjf_ZTCf4As0wZRfNZpB5VjAqqJ45n52ucMDh6-6qSqPmXrmVtbiZIiwRdmWF6DO1riPdTpiu6W1jLG2AX87FLzSePCn6zzIFKWuwFPeeYw5S-iKzJe_H-PeGHzykIhCFIk5Yp9LFeDO-D8ap0dy1faWr-kNRlP4NMe0rzEMjU72SxkFP6XyRlAb11h2did5oJ51VO-Kb1Xcfgfn7O2cpZmvLZ-q6wPLqnyxQ4xCrJmTQOzLUSi7rd09K8HowVei8ltTKxgAAAAHoAeY6AA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
