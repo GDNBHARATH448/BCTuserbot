c1e48ecb2d8f09c8015a34b560850bb68453e846
Chinna op <opchinna742@gmail.com>
2025-04-19 18:05:57 +0530

    Update __init__.py

--- Chinnaop/plugins/__init__.py
+++ Chinnaop/plugins/__init__.py
@@ -1,11 +1,12 @@
 import glob
 from os.path import dirname, isfile
 
-def __list_all_modules():
-    work_dir = dirname(__file__)  # Yeh directory `UTTAM/plugins` ka path hoga
-    mod_paths = glob.glob(work_dir + "/*.py")  # Plugins directory ke andar ke modules
 
-    all_modules = [
+def __list_all_plugins():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
+
+    all_plugins = [
         (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
         if isfile(f)
@@ -13,7 +14,8 @@ def __list_all_modules():
         and not f.endswith("__init__.py")
     ]
 
-    return all_modules
+    return all_plugins
 
-ALL_MODULES = sorted(__list_all_modules())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+
+ALL_PLUGINS = sorted(__list_all_plugins())
+__all__ = ALL_PLUGINS + ["ALL_PLUGINS"]
