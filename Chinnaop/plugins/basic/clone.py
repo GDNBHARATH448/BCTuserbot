bee79c5c7118236b3512919b821d93b78d928568
Chinna op <opchinna742@gmail.com>
2025-04-19 18:54:20 +0530

    Update clone.py

--- Chinnaop/plugins/basic/clone.py
+++ Chinnaop/plugins/basic/clone.py
@@ -1,18 +1,16 @@
 import os
-
 from pyrogram import *
 from pyrogram.types import *
+from ... import app, SUDO_USER
+from ... import *
+from Chinnaop.modules.Chinnaop.basic import edit_or_reply, get_text, get_user
 
 
-from Chinnaop.helper.basic import edit_or_reply, get_text, get_user
-
-from Chinnaop.plugins.help import *
-
-OWNER = os.environ.get("OWNER", "G D N B H A R A T H")
-BIO = os.environ.get("BIO", "ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ ᴀʟᴘʜᴀ @aboutchinnalu")
+OWNER = os.environ.get("OWNER", None)
+BIO = os.environ.get("BIO", "ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ sᴛʀᴀɴɢᴇʀ ᴛᴇᴀᴍ ♡ ᴊᴏɪɴ ɴᴏᴡ @SHIVANSH474")
 
 
-@Client.on_message(filters.command("clone", ".") & filters.me)
+@app.on_message(cdz(["clone"])  & (filters.me | filters.user(SUDO_USER)))
 async def clone(client: Client, message: Message):
     text = get_text(message)
     op = await message.edit_text("`Cloning`")
@@ -36,7 +34,7 @@ async def clone(client: Client, message: Message):
     await message.edit(f"**From now I'm** __{f_name}__")
 
 
-@Client.on_message(filters.command("revert", ".") & filters.me)
+@app.on_message(cdz(["unclone"])  & (filters.me | filters.user(SUDO_USER)))
 async def revert(client: Client, message: Message):
     await message.edit("`Reverting`")
     r_bio = BIO
@@ -52,10 +50,8 @@ async def revert(client: Client, message: Message):
     await message.edit("`I am back!`")
 
 
-add_command_help(
-    "clone",
-    [
-        ["clone", "To Clone someone Profile."],
-        ["revert", "To Get Your Account Back."],
-    ],
-)
+__NAME__ = " Cʟᴏɴᴇ "
+__MENU__ = """
+`.clone` - **reply_message for clone I'd**
+`.unclone` - **reply_message to cloner to get back**
+"""
