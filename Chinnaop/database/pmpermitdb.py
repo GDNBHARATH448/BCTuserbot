a7bad6067ae133fcf61d663d943812684b7c0a4d
Chinna op <opchinna742@gmail.com>
2025-04-19 20:24:11 +0530

    Update pmpermitdb.py

--- Chinnaop/database/pmpermitdb.py
+++ Chinnaop/database/pmpermitdb.py
@@ -1,123 +1,111 @@
-from typing import Dict, List, Union
+import asyncio, random
 
+from pyrogram import filters
+from pyrogram.enums import *
+from pyrogram.types import *
+
+from .clients import app, bot
+from ..mongo.pmguard import *
+from ..mongo.raidzone import *
+from ..strings import raidzone
 from ... import console as vars
-from ..clients.clients import mongodb
 
 
-pmguarddb = mongodb.pmguarddb
-pmallowdb = mongodb.pmallowdb
-pmimagedb = mongodb.pmimagedb
-pmtextsdb = mongodb.pmtextsdb
-pmlimitdb = mongodb.pmlimitdb
-
-
-# PM Permit On/Off
-async def get_pm_permit() -> bool:
-    pm_permit = await pmguarddb.find_one()
-    if not pm_permit:
-        return vars.PM_GUARD
-    get_permit = pm_permit["pm_permit"]
-    return get_permit
-
-
-async def set_pm_permit(permit: bool) -> bool:
-    get_permit = await get_pm_permit()
-    if permit == get_permit:
-        return False
-    await pmguarddb.update_one(
-        {"pm_permit": get_permit},
-        {"$set": {"pm_permit": permit}},
-        upsert=True,
+async def run_async_enums():
+    @app.on_message(
+        (
+            filters.private
+            & filters.incoming
+            & ~filters.service
+            & ~filters.me
+            & ~filters.bot
+            & ~filters.via_bot
+        )
     )
-    return True
+    async def all_events(client, message):
+        check = vars.OLD_MSG
+        flood = vars.FLOODXD
+        user_id = message.from_user.id
+        pm_permit = await get_pm_permit()
+        permit_image = await get_pm_image()
+        permit_text = await get_pm_text()
+        if pm_permit:
+            if not await is_approved_user(user_id):
+                limits = await get_pm_limit()
+                async for m in client.get_chat_history(user_id, limit=limits):
+                    if m.reply_markup:
+                        await m.delete()
+                if str(user_id) in flood:
+                    flood[str(user_id)] += 1
+                else:
+                    flood[str(user_id)] = 1
+                if flood[str(user_id)] > limits:
+                    await message.reply_text("Spammer Detected!\nAutomatic Blocking User!!!")
+                    if str(user_id) in check:
+                        check.pop(str(user_id))
+                        flood.update({user_id: 0})
+                    return await client.block_user(user_id)
+                pm_security = "**🤖 SECURITY WARNING ‼️ ({}/{})**".format(flood[str(user_id)], limits)
+                pm_permit_text = pm_security + "\n\n" + permit_text
+                try:
+                    msg_dlt = await message.reply_photo(
+                        photo=permit_image,
+                        caption=pm_permit_text,
+                    )
+                except Exception:
+                    msg_dlt = await message.reply_text(pm_permit_text)
+                if str(user_id) in check:
+                    try:
+                        await check[str(user_id)].delete()
+                    except BaseException:
+                        pass
+                check[str(user_id)] = msg_dlt
 
 
-
-# approved Users Database
-
-async def is_approved_user(user_id: int) -> bool:
-    user = await pmallowdb.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
-
-
-async def add_approved_user(user_id: int) -> bool:
-    is_approved = await is_approved_user(user_id)
-    if is_approved:
-        return False
-    await pmallowdb.insert_one({"user_id": user_id})
-    return True
-
-
-async def del_approved_user(user_id: int) -> bool:
-    is_approved = await is_approved_user(user_id)
-    if not is_approved:
-        return False
-    await pmallowdb.delete_one({"user_id": user_id})
-    return True
-
-
-async def get_approved_users() -> list:
-    approved_users_list = []
-    async for user in pmallowdb.find({"user_id": {"$gt": 0}}):
-        approved_users_list.append(user)
-    return users_list
-
-
-# PM Image
-async def get_pm_image() -> str:
-    image = await pmimagedb.find_one()
-    if not image:
-        return vars.USERBOT_PICTURE
-    get_image = image["pm_image"]
-    return get_image
-
-
-async def set_pm_image(text: str) -> bool:
-    get_image = await get_pm_image()
-    await pmimagedb.update_one(
-        {"pm_image": get_image},
-        {"$set": {"pm_image": text}},
-        upsert=True,
+    @app.on_message(
+        (
+            filters.incoming
+            & ~filters.service
+            & ~filters.me
+            & ~filters.bot
+            & ~filters.via_bot
+        ),
+        group=1
     )
-    return True
+    async def run_all_events(client, message):
+        try:
+            chat_id = message.chat.id
+            user_id = message.from_user.id
+            cruser = await is_chatraid_user(user_id)
+            lruser = await is_loveraid_user(user_id)
+            fruser = await is_fuckraid_user(user_id)
+        except Exception as e:
+            print(f"Error: {e}")
+            return
 
-
-# PM Text
-async def get_pm_text() -> str:
-    dm_text = await pmtextsdb.find_one()
-    if not dm_text:
-        return vars.PM_GUARD_TEXT
-    get_text = dm_text["pm_text"]
-    return get_text
-
-
-async def set_pm_text(text: str) -> bool:
-    get_text = await get_pm_text()
-    await pmtextsdb.update_one(
-        {"pm_text": get_text},
-        {"$set": {"pm_text": text}},
-        upsert=True,
-    )
-    return True
-
-
-
-# PM Limit
-async def get_pm_limit() -> int:
-    limit = await pmlimitdb.find_one()
-    if not limit:
-        return vars.PM_GUARD_LIMIT
-    get_limit = limit["pm_limit"]
-    return get_image
-
-
-async def set_pm_limit(number: int) -> bool:
-    get_limit = await get_pm_limit()
-    await pmlimitdb.update_one(
-        {"pm_limit": get_limit},
-        {"$set": {"pm_limit": number}},
-        upsert=True,
-    )
-    return True
+        if cruser:
+            pass
+        if lruser:
+            lraid = random.choice(raidzone.LOVERAID)
+            try:
+                await app.send_chat_action(
+                    chat_id,
+                    ChatAction.TYPING,
+                )
+                await asyncio.sleep(3)
+                await message.reply_text(lraid)
+            except Exception as e:
+                # print(f"Error: {e}")
+                pass
+        if fruser:
+            fraid = random.choice(raidzone.GALIRAID)
+            try:
+                await app.send_chat_action(
+                    chat_id,
+                    ChatAction.TYPING,
+                )
+                await asyncio.sleep(3)
+                await message.reply_text(fraid)
+            except Exception as e:
+                # print(f"Error: {e}")
+                pass
