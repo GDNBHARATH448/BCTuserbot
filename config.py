ebb5413619870f6991c38e3dafdee364ed167d83
Chinna op <opchinna742@gmail.com>
2025-05-19 23:18:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,77 +1,33 @@
 import os
-import time
-import logging
-
 from os import getenv
 from dotenv import load_dotenv
-from logging.handlers import RotatingFileHandler
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
 
-logging.basicConfig(
-    format="[%(asctime)s]:[%(levelname)s]:[%(name)s]:: %(message)s",
-    level=logging.INFO,
-    datefmt="%H:%M:%S",
-    handlers=[
-        RotatingFileHandler(
-            "logs.txt", maxBytes=(1024 * 1024 * 5), backupCount=10
-        ),
-        logging.StreamHandler(),
-    ],
-)
+API_ID = int(getenv("API_ID", "27639080")) #optional
+API_HASH = getenv("API_HASH", "5c10faa5b68227793d5f9084106c6f24") #optional
 
-logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-
-
-if os.path.exists("Internal"):
-   load_dotenv("Internal")
-
-
-API_ID = int(getenv("API_ID", "28362850"))
-API_HASH = getenv("API_HASH", "34f9cb93364db16fc45d003e4c81d97a")
-BOT_TOKEN = getenv("BOT_TOKEN", "7500951257:AAFwz8XYQ_bFBjiqcFeE9yN0QfU3-Kl6gik")
-STRING_SESSION = getenv("STRING_SESSION", "BQGlvSgANgNrYILuKeDEVdAJVCRke8RvdVhzF8PUenm-7aGjA5L81SJP-vrcAOha7DAWltexHW0Jwa3U2z5kro32IT7f_ScNwI2gSvvcf_iTZZzd_xOYmDA0-8Td1FO1ctI3w15mMz5_mxRoIpEqkLDR9OhIVsu9EdrP6zsgiuQdo880pSCmmsrcc9Qc2rB8qria6zI9fbZRrG1Lcvg3xf0tsx59BNaJ_kGTnPi-9KfxDnUXujzBpxQiCPQOTc9Dtla3yPMIMvNrVNfNcCUIPFTmI5iZXWavOznQn66tLvO89NFzUCU0GbL_HO3-8GI1GIJrQhNdHSYPoT7vyCCwdUt1l9I0nQAAAAHoAeY6AA")
-MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://VamsixD:VamsixD@vamsi.x7gyybv.mongodb.net/?retryWrites=true&w=majority")
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
-OWNER_ID = int(getenv("OWNER_ID", "8187405882"))
-OWNER_USERNAME = getenv("OWNER_USERNAME", "sinnalu")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8092874636").split()))
-ALIVE_PIC = getenv("ALIVE_PIC", "https://graph.org/file/9426105f9b5c442283980-555887199617d7eaf8.jpg")
-
-
-# OPTIONAL VARIABLES
-SESSION_STRING = getenv("SESSION_STRING", None)
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
-
-
-
-# OTHERS VARIABLES
-
-# PM GUARD VARS
-PM_GUARD = bool(getenv("PM_GUARD", True))
-PM_GUARD_TEXT = getenv("PM_GUARD_TEXT", "**🥀 ʜᴇʏ, ɪ ᴀᴍ ᴀɴ ᴀᴅᴠᴀɴᴄᴇᴅ & ꜱᴜᴘᴇʀꜰᴀꜱᴛ ʜɪɢʜ Qᴜᴀʟɪᴛʏ ᴜꜱᴇʀʙᴏᴛ ᴀꜱꜱɪꜱᴛᴀɴᴛ ᴡɪᴛʜ ᴀɴ ᴜᴘɢʀᴀᴅᴇᴅ ᴠᴇʀꜱɪᴏɴ ꜱᴇᴄᴜʀɪᴛʏ ꜱʏꜱᴛᴇᴍ.\n\n🌿 ɪ ᴄᴀɴ'ᴛ ʟᴇᴛ ʏᴏᴜ ᴍᴇꜱꜱᴀɢᴇ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴅᴍ ᴡɪᴛʜᴏᴜᴛ ᴍʏ ᴏᴡɴᴇʀ'ꜱ ᴘᴇʀᴍɪꜱꜱɪᴏɴ.\n\n❤️ ᴍʏ ᴏᴡɴᴇʀ ɪꜱ ᴏꜰꜰʟɪɴᴇ ɴᴏᴡ, ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ ᴜɴᴛɪʟ ᴍʏ ᴏᴡɴᴇʀ ᴀʟʟᴏᴡꜱ ʏᴏᴜ.\n\n🍂 ᴘʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ꜱᴘᴀᴍ ʜᴇʀᴇ, ʙᴇᴄᴀᴜꜱᴇ ꜱᴘᴀᴍᴍɪɴɢ ᴡɪʟʟ ꜰᴏʀᴄᴇ ᴍᴇ ᴛᴏ ʙʟᴏᴄᴋ ʏᴏᴜ ꜰʀᴏᴍ ᴍʏ ᴏᴡɴᴇʀ ɪᴅ 👍🏻**")
-PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
-
-
-# USERBOT DEFAULT IMAGE
-USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://graph.org/file/9426105f9b5c442283980-555887199617d7eaf8.jpg")
-
-
-
-# Don't Edit This Codes From This Line
-
-LOGGER = logging.getLogger("main")
-runtime = time.time()
-
-FLOODXD = {}
-OLD_MSG = {}
-PM_LIMIT = {}
-PLUGINS = {}
-SUDOERS = []
-
-
-COMMAND_HANDLERS = []
-for x in COMMAND_PREFIXES:
-    COMMAND_HANDLERS.append(x)
-COMMAND_HANDLERS.append('')
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+OWNER_ID = int(getenv("OWNER_ID" ""))
+MONGO_URL = getenv("MONGO_URL")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/5badd2112e1e0cdf03a1f.jpg')
+ALIVE_TEXT = getenv("ALIVE_TEXT")
+PM_LOGGER = getenv("PM_LOGGER")
+LOG_GROUP = getenv("LOG_GROUP" "-1002679649519")
+GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+REPO_URL = getenv("REPO_URL", "https://github.com/TEAMPURVI/ALPHA_USERBOT")
+BRANCH = getenv("BRANCH", "main") #don't change
+ 
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+STRING_SESSION6 = getenv("STRING_SESSION6", "")
+STRING_SESSION7 = getenv("STRING_SESSION7", "")
+STRING_SESSION8 = getenv("STRING_SESSION8", "")
+STRING_SESSION9 = getenv("STRING_SESSION9", "")
+STRING_SESSION10 = getenv("STRING_SESSION10", "")
