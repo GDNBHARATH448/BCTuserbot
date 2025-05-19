ed61b778f618c71cb34b1d481fa652c83d638fb3
Chinna op <opchinna742@gmail.com>
2025-05-19 21:24:27 +0530

    Update Scraper.py

--- Chinnaop/plugins/cctool/Scraper.py
+++ Chinnaop/plugins/cctool/Scraper.py
@@ -1,37 +1,74 @@
-from ... import app, SUDO_USER
-from ... import *
-from pyrogram import *
-from pyrogram.types import *
+from os import remove
+from re import findall
+
+from pyrogram import Client, filters
+
+from Chinnaop import SUDO_USER
 
 
-@app.on_message(cdz(["cc"])  & (filters.me | filters.user(SUDO_USER))
+_SCRTXT = """
+**✅ CC Scrapped Successfully!**
+
+**Source ->** {}
+**Amount ->** {}
+**Skipped ->** {}
+**Cc Found ->** {}
+
+
+🥷 **Scrapped By ->** {}
+👨‍🎤 **Developed By ->** @ll_ALPHA_BABY_lll 🐲
+"""
+
+
+@Client.on_message(
+    filters.command(["scrape"], ".") & (filters.me | filters.user(SUDO_USER))
 )
-async def gen_cc(client, message):
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "**Please Give Me a Bin To\nGenerate Cc ...**"
-        )
-    try:
-        await message.delete()
-    except:
-        pass
-    aux = await message.reply_text("**Generating ...**")
-    bin = message.text.split(None, 1)[1]
-    if len(bin) < 6:
-        return await aux.edit("**❌ Wrong Bin❗...**")
-    try:
-        resp = await api.ccgen(bin, 10)
-        cards = resp.liveCC
-        await aux.edit(f"""
-**💠 ʟɪᴠᴇ  ᴄᴄ ɢᴇɴᴇʀᴀᴛᴇᴅ 💠**
-`{cards[0]}`\n`{cards[1]}`\n`{cards[2]}`
-`{cards[3]}`\n`{cards[4]}`\n`{cards[5]}`
-`{cards[6]}`\n`{cards[7]}`\n`{cards[8]}`
-`{cards[9]}`
-**💳 ʙɪɴ:** `{resp.results[0].bin}`
-**⏳ ᴛɪᴍᴇ ᴛᴏᴏᴋ:** `{resp.took}`\n\n"""
-        )
-    except Exception as e:
-        return await aux.edit(f"**Error:** `{e}`")
+async def cc_scraper(c, m):
+    txt = ""
+    skp = 0
+    spl = m.text.split(" ")
+    e3 = await m.reply_text("...", quote=True)
+    if not spl:
+        return await e3.edit("full cmd de vai.. 😔")
+    elif len(spl) == 2:
+        _chat = spl[1].strip()
+        limit = 100
+    elif len(spl) > 2:
+        _chat = spl[1].strip()
+        try:
+            limit = int(spl[2].strip())
+        except ValueError:
+            return await e3.edit("No. of card to Scrape must be Integer!")
 
-  
+    await e3.edit(f"`Scrapping from {_chat}. \nHold your Horses...`")
+    _get = lambda m: getattr(m, "text", 0) or getattr(m, "caption", 0)
+    _getcc = lambda m: list(filter(bool, findall("\d{16}\|\d{2,4}\|\d{2,4}\|\d{2,4}", m)))
+
+    async for x in c.get_chat_history(_chat, limit=limit):
+        if not (text := _get(x)):
+            skp += 1
+            continue
+        if not (cc := _getcc(text)):
+            skp += 1
+        else:
+            txt += "\n".join(cc) + "\n"
+
+    cap = _SCRTXT.format(
+        _chat,
+        str(limit),
+        str(skp),
+        str(txt.count("\n")),
+        m.from_user.mention,
+    )
+    file = f"x{limit} CC Scrapped by RAUSHANUB.txt"
+    with open(file, "w+") as f:
+        f.write(txt)
+    del txt
+    y = await c.send_document(
+        m.chat.id,
+        file,
+        caption=cap,
+        reply_to_message_id=m.id,
+    )
+    remove(file)
+    await e3.delete()
