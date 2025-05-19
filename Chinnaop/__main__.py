bc31a320ec622eefc54ad0073ce2af4f49da07c4
Chinna op <opchinna742@gmail.com>
2025-05-19 20:06:54 +0530

    Update __main__.py

--- Chinnaop/__main__.py
+++ Chinnaop/__main__.py
@@ -1,42 +1,26 @@
-import asyncio, importlib
-
+import asyncio
+import importlib
 from pyrogram import Client, idle
+from Chinnaop.helper import join
+from Chinnaop.modules import ALL_MODULES
+from Chinnaop import clients, app, ids
 
-from . import logs, plugs, vars
-from .plugins import ALL_PLUGINS
-from .modules.clients.clients import run_async_clients
-from .modules.clients.enums import run_async_enums
-from .modules.helpers.inline import run_async_inline
-
-
-async def main():
-    await run_async_clients()
-    for all_plugin in ALL_PLUGINS:
-        imported_plugin = importlib.import_module(
-            "Chinnaop.plugins" + all_plugin
-        )
-        if (hasattr
-            (
-                imported_plugin, "__NAME__"
-            ) and imported_plugin.__NAME__
-        ):
-            imported_plugin.__NAME__ = imported_plugin.__NAME__
-            if (
-                hasattr(
-                    imported_plugin, "__MENU__"
-                ) and imported_plugin.__MENU__
-            ):
-                plugs[imported_plugin.__NAME__.lower()
-                ] = imported_plugin
-    await run_async_enums()
-    logs.info(">> Successfully Imported All Plugins.")
-    await run_async_inline()
-    logs.info("Successfully Deployed !!")
-    logs.info("Do Visit - @MASTIWITHFRIENDSXD")
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting Zeus.")
+    for all_module in ALL_MODULES:
+        importlib.import_module("Chinnaop.modules" + all_module)
+        print(f"Successfully Imported {all_module} 💥")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            await join(cli)
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
     await idle()
 
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    print("Userbot Stopped !\nGoodBye ...")
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
