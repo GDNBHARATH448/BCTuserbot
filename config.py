c66f34271b9ca6b1cc224b2ea6357e0f27b74bed
Chinna op <opchinna742@gmail.com>
2025-05-19 23:21:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ if os.path.exists("local.env"):
 API_ID = int(getenv("API_ID", "27639080")) #optional
 API_HASH = getenv("API_HASH", "5c10faa5b68227793d5f9084106c6f24") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID" ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7928209707").split()))
+OWNER_ID = int(getenv("OWNER_ID", "7928209707"))
+MONGO_URL = getenv("MONGO_URL", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7500951257:AAHBI0QU0AlnM8q413Xx0nIruyqgpRYblaY")
 ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/5badd2112e1e0cdf03a1f.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
@@ -21,7 +21,7 @@ GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/TEAMPURVI/ALPHA_USERBOT")
 BRANCH = getenv("BRANCH", "main") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQGlvSgANgNrYILuKeDEVdAJVCRke8RvdVhzF8PUenm-7aGjA5L81SJP-vrcAOha7DAWltexHW0Jwa3U2z5kro32IT7f_ScNwI2gSvvcf_iTZZzd_xOYmDA0-8Td1FO1ctI3w15mMz5_mxRoIpEqkLDR9OhIVsu9EdrP6zsgiuQdo880pSCmmsrcc9Qc2rB8qria6zI9fbZRrG1Lcvg3xf0tsx59BNaJ_kGTnPi-9KfxDnUXujzBpxQiCPQOTc9Dtla3yPMIMvNrVNfNcCUIPFTmI5iZXWavOznQn66tLvO89NFzUCU0GbL_HO3-8GI1GIJrQhNdHSYPoT7vyCCwdUt1l9I0nQAAAAHoAeY6AA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
