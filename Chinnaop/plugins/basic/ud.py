f1198c8c259d94b2371159d7cbaeab385511ca22
Chinna op <opchinna742@gmail.com>
2025-05-20 13:23:41 +0530

    Update ud.py

--- Chinnaop/plugins/basic/ud.py
+++ Chinnaop/plugins/basic/ud.py
@@ -1,56 +1,47 @@
-from ... import *
-from pyrogram import Client, filters
+from asyncio import sleep
 
-from pyrogram.enums import ParseMode
+from pyrogram import filters, Client 
 
-####
 
-@app.on_message(cdz(["id"]) & filters.me)
-async def getid(client, message):
-    chat = message.chat
-    your_id = message.from_user.id
-    message_id = message.id
-    reply = message.reply_to_message
+from Chinnaop.helper.aiohttp_helper import AioHttp
+from Chinnaop.plugins.help import add_command_help
 
-    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
-    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
 
-    if not message.command:
-        message.command = message.text.split()
+def replace_text(text):
+    return text.replace('"', "").replace("\\r", "").replace("\\n", "").replace("\\", "")
 
-    if not message.command:
-        message.command = message.text.split()
 
-    if len(message.command) == 2:
-        try:
-            split = message.text.split(None, 1)[1].strip()
-            user_id = (await client.get_users(split)).id
-            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user_id})** `{user_id}`\n"
+@Client.on_message(filters.me & filters.command(["ud"], "."))
+async def urban_dictionary(bot, message):
+    if len(message.text.split()) == 1:
+        await message.edit("Usage: `ud example`")
+        return
+    try:
+        text = message.text.split(None, 1)[1]
+        response = await AioHttp().get_json(
+            f"http://api.urbandictionary.com/v0/define?term={text}"
+        )
+        word = response["list"][0]["word"]
+        definition = response["list"][0]["definition"]
+        example = response["list"][0]["example"]
+        resp = (
+            f"**Text: {replace_text(word)}**\n"
+            f"**Meaning:**\n`{replace_text(definition)}`\n\n"
+            f"**Example:**\n`{replace_text(example)}` "
+        )
+        await message.edit(resp)
+        return
+    except Exception as e:
+        await message.edit("`The Urban Dictionary API could not be reached`")
+        print(e)
+        await sleep(3)
+        await message.delete()
 
-        except Exception:
-            return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
 
-    text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
 
-    if (
-        not getattr(reply, "empty", True)
-        and not message.forward_from_chat
-        and not reply.sender_chat
-    ):
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
-
-    if reply and reply.forward_from_chat:
-        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
-        print(reply.forward_from_chat)
-
-    if reply and reply.sender_chat:
-        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
-        print(reply.sender_chat)
-
-    await message.reply_text(
-        text,
-        disable_web_page_preview=True,
-        parse_mode=ParseMode.DEFAULT,
-          )
-                                                                                         
+add_command_help(
+    "dictionary",
+    [
+        [".ubran | .ud", "Define the word you send or reply to."],
+    ],
+        )
