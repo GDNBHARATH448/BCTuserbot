b7061d071542607f7c0be1d47eb8e6d12ef474bc
Chinna op <opchinna742@gmail.com>
2025-05-20 12:56:32 +0530

    Update Vctool.py

--- Chinnaop/plugins/basic/Vctool.py
+++ Chinnaop/plugins/basic/Vctool.py
@@ -1,111 +1,97 @@
-from ... import *
-from pyrogram import filters
+from random import randint
+from typing import Optional
+
+from pyrogram import Client, enums, filters
 from pyrogram.raw.functions.channels import GetFullChannel
 from pyrogram.raw.functions.messages import GetFullChat
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
+from pyrogram.types import Message
+from Chinnaop import SUDO_USER
+from pyrogram.types import Message
+from Chinnaop.plugins.help import add_command_help
 
 
-async def get_vc_call(client, message):
-    chat_id = message.chat.id
-    chat_peer = await client.resolve_peer(chat_id)
-    if isinstance(chat_peer,
-        (InputPeerChannel, InputPeerChat)
-    ):
+def get_arg(message: Message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+async def get_group_call(
+    client: Client, message: Message, err_msg: str = ""
+) -> Optional[InputGroupCall]:
+    chat_peer = await client.resolve_peer(message.chat.id)
+    if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
         if isinstance(chat_peer, InputPeerChannel):
             full_chat = (
-                await client.invoke(
-                    GetFullChannel(channel=chat_peer)
-                )
+                await client.invoke(GetFullChannel(channel=chat_peer))
             ).full_chat
         elif isinstance(chat_peer, InputPeerChat):
             full_chat = (
-                await client.invoke(
-                    GetFullChat(chat_id=chat_peer.chat_id)
-                )
+                await client.invoke(GetFullChat(chat_id=chat_peer.chat_id))
             ).full_chat
-            
         if full_chat is not None:
             return full_chat.call
-            
+    await message.edit(f"**No group call Found** {err_msg}")
     return False
 
-@app.on_message(cdx(["svc", "startvc"]) & ~filters.private)
-@sudo_users_only
-async def create_video_chat(client, message):
+
+@Client.on_message(
+    filters.command(["startvc"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def opengc(client: Client, message: Message):
+    flags = " ".join(message.command[1:])
+    tex = await message.reply_text(message, "`Processing . . .`")
+    vctitle = get_arg(message)
+    if flags == enums.ChatType.CHANNEL:
+        chat_id = message.chat.title
+    else:
+        chat_id = message.chat.id
+    args = f"**Started Group Call\n • **Chat ID** : `{chat_id}`"
+    try:
+        if not vctitle:
+            await client.invoke(
+                CreateGroupCall(
+                    peer=(await client.resolve_peer(chat_id)),
+                    random_id=randint(10000, 999999999),
+                )
+            )
+        else:
+            args += f"\n • **Title:** `{vctitle}`"
+            await client.invoke(
+                CreateGroupCall(
+                    peer=(await client.resolve_peer(chat_id)),
+                    random_id=randint(10000, 999999999),
+                    title=vctitle,
+                )
+            )
+        await tex.edit(args)
+    except Exception as e:
+        await tex.edit(f"**INFO:** `{e}`")
+
+
+@Client.on_message(
+    filters.command(["stopvc"], ".") & (filters.me | filters.user(SUDO_USER))
+)
+async def end_vc_(client: Client, message: Message):
     chat_id = message.chat.id
-    try:
-        aux = await eor(message, "**🔄 Processing ...**")
-        vc_call = await get_vc_call(client, message)
-        if vc_call:
-            return await aux.edit("**🤖 VC Already Active❗**")
-        peer = await client.resolve_peer(chat_id)
-        await client.invoke(
-            CreateGroupCall(
-                peer=peer,
-                random_id=client.rnd_id() // 9000000000,
-            ),
+    if not (
+        group_call := (
+            await get_group_call(client, message, err_msg=", group call already ended")
         )
-        await aux.edit("**🤖 Successfully Started VC. 🌿**")
-    except Exception as e:
-        print(f"Error: {e}")
-        pass
+    ):
+        return
+    await client.invoke(DiscardGroupCall(call=group_call))
+    await message.reply_text(f"Ended group call in **Chat ID** : `{chat_id}`")
 
 
-
-@app.on_message(cdx(["dvc", "evc", "stopvc", "endvc"]) & ~filters.private)
-@sudo_users_only
-async def discard_video_chat(client, message):
-    user_id = message.from_user.id
-    try:
-        aux = await eor(message, "**🔄 Processing ...**")
-        vc_call = await get_vc_call(client, message)
-        if not vc_call:
-            return await aux.edit("**🤖 VC Not Started Yet❗**")
-        await client.invoke(
-            DiscardGroupCall(call=vc_call)
-        )
-        return await aux.edit("**🤖 Succesfully Ended VC. 🌿**")
-    except Exception as e:
-        print(f"Error: {e}")
-        pass
-
-
-@app.on_message(cdx(["rvc", "restartvc"]) & ~filters.private)
-@sudo_users_only
-async def discard_video_chat(client, message):
-    chat_id = message.chat.id
-    try:
-        aux = await eor(message, "**🔄 Processing ...**")
-        vc_call = await get_vc_call(client, message)
-        if not vc_call:
-            return await aux.edit("**🤖 VC Not Started Yet❗**")
-        peer = await client.resolve_peer(chat_id)
-        await client.invoke(
-            DiscardGroupCall(call=vc_call)
-        )
-        await aux.edit("**🤖 Succesfully Ended VC. 🌿**")
-        await client.invoke(
-            CreateGroupCall(
-                peer=peer,
-                random_id=client.rnd_id() // 9000000000,
-            ),
-        )
-        return await aux.edit("**🤖 Succesfully Restarted VC. 🌿**")
-    except Exception as e:
-        print(f"Error: {e}")
-        pass
-
-
-
-
-__NAME__ = "Vᴄ"
-__MENU__ = """
-**Start or End VC in Your Channel
-Or Group By Simple Commands.**
-
-`.svc` - Start VC in Your Chat.
-`.dvc` - End Vc in Your Chat.
-`.rvc` - Restart VC in Your Chat
-"""
-  
+add_command_help(
+    "vctools",
+    [
+        ["startvc", "Start voice chat of group."],
+        ["stopvc", "End voice chat of group."],
+    ],
+)
