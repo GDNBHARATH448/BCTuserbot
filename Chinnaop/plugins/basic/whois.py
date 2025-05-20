f33abf60382ff066de024057355cd9e7759127da
Chinna op <opchinna742@gmail.com>
2025-05-20 13:05:34 +0530

    Update whois.py

--- Chinnaop/plugins/basic/whois.py
+++ Chinnaop/plugins/basic/whois.py
@@ -1,38 +1,149 @@
-from pyrogram import filters
-import asyncio
-import pyfiglet 
-from random import choice
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
-from pyrogram.handlers import MessageHandler
-from ... import *
+from asyncio import gather
+from os import remove
 
-def figle(text):
-    x = pyfiglet.FigletFont.getFonts()
-    font = choice(x)
-    figled = str(pyfiglet.figlet_format(text,font=font))
-    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(text="ᴄʜᴀɴɢᴇ", callback_data="figlet"),InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close_reply")]])
-    return figled, keyboard
+from pyrogram import Client, filters
+from pyrogram.enums import ChatType
+from pyrogram.types import Message
 
-@app.on_message(cdz(["write"]) & filters.me)
-async def echo(bot, message):
-    global text
+from Chinnaop.helper.PyroHelpers import ReplyCheck
+from Chinnaop.plugins.basic.profile import extract_user
+
+from Chinnaop.plugins.help import add_command_help
+
+
+@Client.on_message(filters.command(["whois", "info"], ".") & filters.me)
+async def who_is(client: Client, message: Message):
+    user_id = await extract_user(message)
+    ex = await message.edit_text("`Processing . . .`")
+    if not user_id:
+        return await ex.edit(
+            "**Provide userid/username/reply to get that user's info.**"
+        )
     try:
-        text = message.text.split(' ',1)[1]
-    except IndexError:
-        return await message.reply_text("Example:\n\n`/figlet SHASHANK PAPA `")
-    kul_text, keyboard = figle(text)
-    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ :\n<pre>{kul_text}</pre>", quote=True, reply_markup=keyboard)
+        user = await client.get_users(user_id)
+        username = f"@{user.username}" if user.username else "-"
+        first_name = f"{user.first_name}" if user.first_name else "-"
+        last_name = f"{user.last_name}" if user.last_name else "-"
+        fullname = (
+            f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
+        )
+        user_details = (await client.get_chat(user.id)).bio
+        bio = f"{user_details}" if user_details else "-"
+        h = f"{user.status}"
+        if h.startswith("UserStatus"):
+            y = h.replace("UserStatus.", "")
+            status = y.capitalize()
+        else:
+            status = "-"
+        dc_id = f"{user.dc_id}" if user.dc_id else "-"
+        common = await client.get_common_chats(user.id)
+        out_str = f"""<b>USER INFORMATION:</b>
 
-@app.on_callback_query(filters.regex("figlet"))
-async def figlet_handler(Client, query: CallbackQuery):
-  try:
-      kul_text, keyboard = figle(text)
-      await query.message.edit_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ғɪɢʟᴇᴛ :\n<pre>{kul_text}</pre>", reply_markup=keyboard)
-  except Exception as e : 
-      await message.reply(e)
+🆔 <b>User ID:</b> <code>{user.id}</code>
+👤 <b>First Name:</b> {first_name}
+🗣️ <b>Last Name:</b> {last_name}
+🌐 <b>Username:</b> {username}
+🏛️ <b>DC ID:</b> <code>{dc_id}</code>
+🤖 <b>Is Bot:</b> <code>{user.is_bot}</code>
+🚷 <b>Is Scam:</b> <code>{user.is_scam}</code>
+🚫 <b>Restricted:</b> <code>{user.is_restricted}</code>
+✅ <b>Verified:</b> <code>{user.is_verified}</code>
+⭐ <b>Premium:</b> <code>{user.is_premium}</code>
+📝 <b>User Bio:</b> {bio}
 
-
-__NAME__ = "Fɪɢʟᴇᴛ"
-__MENU__ = """
-`.write` - **text to figlet.**
+👀 <b>Same groups seen:</b> {len(common)}
+👁️ <b>Last Seen:</b> <code>{status}</code>
+🔗 <b>User permanent link:</b> <a href='tg://user?id={user.id}'>{fullname}</a>
 """
+        photo_id = user.photo.big_file_id if user.photo else None
+        if photo_id:
+            photo = await client.download_media(photo_id)
+            await gather(
+                ex.delete(),
+                client.send_photo(
+                    message.chat.id,
+                    photo,
+                    caption=out_str,
+                    reply_to_message_id=ReplyCheck(message),
+                ),
+            )
+            remove(photo)
+        else:
+            await ex.edit(out_str, disable_web_page_preview=True)
+    except Exception as e:
+        return await ex.edit(f"**INFO:** `{e}`")
+
+
+@Client.on_message(filters.command(["chatinfo", "cinfo", "ginfo"], ".") & filters.me)
+async def chatinfo_handler(client: Client, message: Message):
+    ex = await message.edit_text("`Processing...`")
+    try:
+        if len(message.command) > 1:
+            chat_u = message.command[1]
+            chat = await client.get_chat(chat_u)
+        else:
+            if message.chat.type == ChatType.PRIVATE:
+                return await message.edit(
+                    f"Use this command within a group or use .chatinfo [group username or id]`"
+                )
+            else:
+                chatid = message.chat.id
+                chat = await client.get_chat(chatid)
+        h = f"{chat.type}"
+        if h.startswith("ChatType"):
+            y = h.replace("ChatType.", "")
+            type = y.capitalize()
+        else:
+            type = "Private"
+        username = f"@{chat.username}" if chat.username else "-"
+        description = f"{chat.description}" if chat.description else "-"
+        dc_id = f"{chat.dc_id}" if chat.dc_id else "-"
+        out_str = f"""<b>CHAT INFORMATION:</b>
+
+🆔 <b>Chat ID:</b> <code>{chat.id}</code>
+👥 <b>Title:</b> {chat.title}
+👥 <b>Username:</b> {username}
+📩 <b>Type:</b> <code>{type}</code>
+🏛️ <b>DC ID:</b> <code>{dc_id}</code>
+🗣️ <b>Is Scam:</b> <code>{chat.is_scam}</code>
+🎭 <b>Is Fake:</b> <code>{chat.is_fake}</code>
+✅ <b>Verified:</b> <code>{chat.is_verified}</code>
+🚫 <b>Restricted:</b> <code>{chat.is_restricted}</code>
+🔰 <b>Protected:</b> <code>{chat.has_protected_content}</code>
+
+🚻 <b>Total members:</b> <code>{chat.members_count}</code>
+📝 <b>Description:</b>
+<code>{description}</code>
+"""
+        photo_id = chat.photo.big_file_id if chat.photo else None
+        if photo_id:
+            photo = await client.download_media(photo_id)
+            await gather(
+                ex.delete(),
+                client.send_photo(
+                    message.chat.id,
+                    photo,
+                    caption=out_str,
+                    reply_to_message_id=ReplyCheck(message),
+                ),
+            )
+            remove(photo)
+        else:
+            await ex.edit(out_str, disable_web_page_preview=True)
+    except Exception as e:
+        return await ex.edit(f"**INFO:** `{e}`")
+
+
+add_command_help(
+    "info",
+    [
+        [
+            "info <username/userid/reply>",
+            "get telegram user info with full description.",
+        ],
+        [
+            "chatinfo <username/chatid/reply>",
+            "get group info with full description.",
+        ],
+    ],
+        )
