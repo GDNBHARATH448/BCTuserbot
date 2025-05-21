87108739bec8051181bab613d359998eb58543f9
Chinna op <opchinna742@gmail.com>
2025-05-21 09:41:13 +0530

    Update rraid.py

--- Chinnaop/database/rraid.py
+++ Chinnaop/database/rraid.py
@@ -1,88 +1,24 @@
-from typing import Dict, List, Union
+from Chinnaop.database import cli
 
-from ... import console as vars
-from ..clients.clients import mongodb
-
-
-chatraiddb = mongodb.chatraiddb
-fuckraiddb = mongodb.fuckraiddb
-loveraiddb = mongodb.loveraiddb
+collection = cli["Chinnaop"]["rraid"]
 
 
-
-# ChatRaid Users Database
-
-async def is_chatraid_user(user_id: int) -> bool:
-    user = await chatraiddb.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
-
-
-async def add_chatraid_user(user_id: int) -> bool:
-    is_chatraid = await is_chatraid_user(user_id)
-    if is_chatraid:
-        return False
-    await chatraiddb.insert_one({"user_id": user_id})
-    return True
+async def rraid_user(chat):
+    doc = {"_id": "Rraid", "users": [chat]}
+    r = await collection.find_one({"_id": "Rraid"})
+    if r:
+        await collection.update_one({"_id": "Rraid"}, {"$push": {"users": chat}})
+    else:
+        await collection.insert_one(doc)
 
 
-async def del_chatraid_user(user_id: int) -> bool:
-    is_chatraid = await is_chatraid_user(user_id)
-    if not is_chatraid:
-        return False
-    await chatraiddb.delete_one({"user_id": user_id})
-    return True
+async def get_rraid_users():
+    results = await collection.find_one({"_id": "Rraid"})
+    if results:
+        return results["users"]
+    else:
+        return []
 
 
-
-
-# FuckRaid Users Database
-
-async def is_fuckraid_user(user_id: int) -> bool:
-    user = await fuckraiddb.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
-
-
-async def add_fuckraid_user(user_id: int) -> bool:
-    is_fuckraid = await is_fuckraid_user(user_id)
-    if is_fuckraid:
-        return False
-    await fuckraiddb.insert_one({"user_id": user_id})
-    return True
-
-
-async def del_fuckraid_user(user_id: int) -> bool:
-    is_fuckraid = await is_fuckraid_user(user_id)
-    if not is_fuckraid:
-        return False
-    await fuckraiddb.delete_one({"user_id": user_id})
-    return True
-
-
-
-# LoveRaid Users Database
-
-async def is_loveraid_user(user_id: int) -> bool:
-    user = await loveraiddb.find_one({"user_id": user_id})
-    if not user:
-        return False
-    return True
-
-
-async def add_loveraid_user(user_id: int) -> bool:
-    is_loveraid = await is_loveraid_user(user_id)
-    if is_loveraid:
-        return False
-    await loveraiddb.insert_one({"user_id": user_id})
-    return True
-
-
-async def del_loveraid_user(user_id: int) -> bool:
-    is_loveraid = await is_loveraid_user(user_id)
-    if not is_loveraid:
-        return False
-    await loveraiddb.delete_one({"user_id": user_id})
-    return True
+async def unrraid_user(chat):
+    await collection.update_one({"_id": "Rraid"}, {"$pull": {"users": chat}})
